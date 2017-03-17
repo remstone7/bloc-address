ec1d96f890b0368f1e4fce13620178b8789d1eb7
Rem Stone <remstone7@gmail.com>
2017-03-17 19:44:38 -0400

    create main menu

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,8 @@
+require_relative 'controllers/menu_controller'
+# create new menu controller
+menu = MenuController.new
+# clear cmd line
+system "clear"
 puts "Welcome to Address Bloc"
+# display main_menu
+menu.main_menu
